0ca4ae350b358076e06ca1d41300b086a3a499fa
jackwilmerding <61218012+jackwilmerding@users.noreply.github.com>
2020-06-13 16:07:43 -0400

    Add files via upload

---
+++ Backend.java
@@ -0,0 +1,64 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.io.File;
+import java.util.Calendar;
+public class Backend {
+    public static final String DATE_FORMAT_NOW = "yyyy-MM-dd";
+    public static String date() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+        return sdf.format(cal.getTime());
+    }
+    public HashMap<String, Patron> reader(String queryDate) {
+        HashMap hm = new HashMap<String, Patron>();
+        Scanner scan = new Scanner(date() + ".csv");
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] data = line.split(",");
+            hm.put(data[0], new Patron(data[1], data[2], data[0]));
+        }
+        return hm;
+    }
+    public void writer(String firstName, String lastName, String email) throws IOException {
+        File file = new File(date() + ".csv");
+        if(file.exists() && !file.isDirectory()) {
+            String csv = "\n" + email + "," + firstName + "," + lastName;
+            FileWriter fwriter = new FileWriter(date() + ".csv", true);
+            fwriter.write(csv);
+            fwriter.flush();
+            fwriter.close();
+        }
+        else {
+            FileWriter fWriter = new FileWriter(date() + ".csv");
+            String info = email + "," + firstName + "," + lastName;
+            System.out.print(info);
+            fWriter.write(info);
+            fWriter.flush();
+            fWriter.close();
+        }
+        return;
+    }
+    public String[] search(String email) {
+        File dir = new File("ACCT 2/");
+        File[] files = dir.listFiles();
+        //TODO this is a null pointer exception for files
+        ArrayList<String> dates = new ArrayList<String>();
+        for(File file : files) {
+            if (file != null && file.getName().endsWith(".csv")) {
+                Scanner scan = new Scanner(file.getName());
+                while(scan.hasNextLine()) {
+                    String line = scan.nextLine();
+                    String[] data = line.split(",");
+                    if (data[0].equals(email)) {
+                        dates.add(data[0]);
+                    }
+                }
+            }
+        }
+        return dates.toArray(new String[dates.size()]);
+    }
+}
---
+++ Display.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+public class Display extends JFrame {
+
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 400;
+
+    JPanel cards;
+
+    public Display() {
+        super("ACCT 2");
+
+        cards = new JPanel(new CardLayout());
+        cards.add(new MainMenu(cards), "Main Menu");
+        cards.add(new CustomerLogger(cards), "Customer Logger");
+        cards.add(new IncidentReport(cards), "Incident Reporter");
+        add(cards);
+
+        setSize(WIDTH, HEIGHT);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        new Display();
+    }
+}
